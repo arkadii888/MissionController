615c3b54a450b8ca000f2885593c341513cbae99
arkadii <arkadii.personal@proton.me>
2026-04-19 20:22:55 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,11 @@ int main() {
 
     ConnectionResult connectionResult = mavsdk.add_any_connection("serial:///dev/ttyAMA0:921600");
     if (connectionResult != ConnectionResult::Success) {
-        std::cout << "Adding Connection Failed: " << static_cast<int>(connectionResult) << '\n';
+        std::cout << "Adding Connection Failed: " << static_cast<int>(connectionResult) << std::endl;
         return 1;
     }
 
     while(mavsdk.systems().empty()) {
-        std::cout << "Waiting for Systems Collection..." << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
