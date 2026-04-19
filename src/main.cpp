572c59bc30bd67a682787b04d6be841534f46cf7
arkadii <arkadii.personal@proton.me>
2026-04-19 17:57:24 +0200

    adding connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+#include "mavsdk/mavsdk.h"
 
 int main() {
+    Mavsdk mavsdk{Mavsdk::Configuration{Mavsdk::ComponentType::GroundStation}};
+    ConnectionResult connectionResult = mavsdk.add_any_connection("serial:///dev/ttyAMA0:921600");
+    if (connectionResult != ConnectionResult::Success) {
+        std::cout << "Adding Connection Failed: " << connectionResult << '\n';
+        return 1;
+    }
+
     return 0;
 }
