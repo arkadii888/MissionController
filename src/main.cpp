a72675dd5a0b08e9c5e9920705795eef1fa48338
arkadii <arkadii.personal@proton.me>
2026-04-19 19:26:52 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "mavsdk/mavsdk.h"
-//#include "mavsdk/mavsdk.hpp"
 
 using namespace mavsdk;
 
@@ -8,19 +7,16 @@ int main() {
     Mavsdk mavsdk{Mavsdk::Configuration{ComponentType::GroundStation}};
     ConnectionResult connectionResult = mavsdk.add_any_connection("serial:///dev/ttyAMA0:921600");
     if (connectionResult != ConnectionResult::Success) {
-        std::cout << "Adding Connection Failed: " << connectionResult << '\n';
+        std::cout << "Adding Connection Failed: " << static_cast<int>(connectionResult) << '\n';
         return 1;
     }
 
     auto systems = mavsdk.systems();
     for(auto s : systems) {
         if(s) {
-            std::cout << s->vehicle_type() << std::endl;
+            std::cout << "Vehicle Type: " << static_cast<int>(s->vehicle_type()) << std::endl;
         }
     }
-
-    //auto vehicle = mavsdk.systems()[0];
-    //std::cout << "Vehicle Type: " << vehicle->vehicle_type() << std::endl;
 
     return 0;
 }
