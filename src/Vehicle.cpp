b57ec6f03450dc6763db166139b6f81b76a6784e
arkadii <arkadii.personal@proton.me>
2026-05-09 16:57:29 +0200

    Update Vehicle.cpp

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -60,11 +60,11 @@ void Vehicle::Arm() {
 }
 
 void Vehicle::StartMission(const std::vector<mavsdk::Mission::MissionItem>& missionItems) {
-    auto progress = mission->mission_progress();
-    if(progress.total > 0) {
-        Hold();
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
+    //auto progress = mission->mission_progress();
+    //if(progress.total > 0) {
+    //    Hold();
+    //    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    //}
 
     try {
         ClearMission(); // TODO: Fake, if missions list is empty it returns error
